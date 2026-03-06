1538e1726c794b5e2b45306429a239e5add49a8c
virat <viratt737@gmail.com>
2026-03-06 15:26:54 +0530

    write rider controller for register rider

--- Backend/controllers/rider.controller.js
+++ Backend/controllers/rider.controller.js
@@ -1,2 +1,33 @@
 const riderModel = require('../Models/rider.model');
 const riderService = require('../service/rider.service');
+const{validationResult} = require('express-validator');
+
+
+module.exports.registerRider = async (req, res, next) =>{
+    
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(40).json({
+            errors: errors.array()
+        });
+    }
+
+    const {fullname, email, password, vehicle} = req.body;
+    
+    const isRiderAlreadyExist = await riderModel.findOne({email});
+    const hashedPassword = await riderModel.hashPassword(password);
+
+    const rider = await riderService.createRider({
+        firstname : fullname.firstname,
+        lastname : fullname.lastname,
+        email,
+        password: hashedPassword,
+        color : vehicle.color,
+        plate : vehicle.plate,
+        capacity: vehicle.capacity,
+        vehicleType : vehicle.vehicleType
+    });
+
+    const token = rider.generateAuthToken();
+    res.status(201).json({token , rider});
+}
