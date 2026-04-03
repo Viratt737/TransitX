a3fb09e68effb3703e65ed1c548c9f4157f7f09d
virat <viratt737@gmail.com>
2026-04-04 03:37:49 +0530

    "try to create uber ui with dummy data"

--- Frontend/src/components/LocationSearchPanel.jsx
+++ Frontend/src/components/LocationSearchPanel.jsx
@@ -1,28 +1,25 @@
 import React from 'react'
 
 const LocationSearchPanel = () => {
+    const loactions = [
+        "4/184 Ambedkarpuram awas vikas kalyanpur kanpur",
+        "1st Floor, 118/330, Kaushalpuri, Gumti Number 5, Kanpur, Uttar Pradesh, 208012.",
+        "118/163-B, Kaushalpuri, Kanpur Nagar, Uttar Pradesh, 208012.",
+        "Commercial area (near Octave, Red Chief), Gumti No 5, Kanpur."
+    ]
   return (
     <div>
-        <div className='flex gap-4 items-center my-2 justify-start'>
+         {
+            loactions.map(function(elem){
+                return  <div className='flex gap-4 border-2 p-3 border-white active:border-black rounded-xl items-center my-2 justify-start'>
             <h2 className='bg-white h-10 flex items-center justify-center w-16 rounded-full'>
                 <i className="ri-map-pin-line"></i>
             </h2>
-            <h5 className='text-lg font-medium'>4/184 Ambedkarpuram awas vikas kalyanpur kanpur</h5>
+            <h5 className='text-lg font-medium'>{elem}</h5>
         </div>
-
-        <div className='flex gap-4 items-center my-4 justify-start'>
-            <h2 className='bg-white h-10 flex items-center justify-center w-16 rounded-full'>
-                <i className="ri-map-pin-line"></i>
-            </h2>
-            <h5 className='text-lg font-medium'>4/184 Ambedkarpuram awas vikas kalyanpur kanpur</h5>
-        </div>
-
-        <div className='flex gap-4 items-center my-4 justify-start'>
-            <h2 className='bg-white h-10 flex items-center justify-center w-16 rounded-full'>
-                <i className="ri-map-pin-line"></i>
-            </h2>
-            <h5 className='text-lg font-medium'>4/184 Ambedkarpuram awas vikas kalyanpur kanpur</h5>
-        </div>
+            })
+         }
+        
     </div>
 
     
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -67,7 +67,7 @@ function Home() {
         <form onSubmit={(e) =>{
             submitHandler(e)
         }}>
-           <div className='line absolute h-16 w-1 top-[45%] left-10 bg-gray-700 rounded-full'></div>
+           <div className='line absolute h-12 w-1 top-[45%] left-7 bg-gray-700 rounded-full'></div>
           <input 
             onClick={() =>{
               setPanelOpen(true)
@@ -99,9 +99,10 @@ function Home() {
         </div>
       </div>
       
-      <div className='fixed z-10 bottom-0 w-full bg-white px-3 py-6'>
+      <div className='fixed z-10 bottom-0 translate-y-full w-full bg-white px-3 py-6'>
          <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
-         <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+
+         <div className='flex border-2 active:border-black bg-gray-100 mb-2 rounded-xl w-full p-3 items-center justify-between'>
             <img className='h-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxQLjsY-4pvrxwCn0QWDCGljnRsqqc1CpF-Q&s" alt="" />
              <div className='w-1/2'>
                 <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-line"></i>4</span></h4>
@@ -112,7 +113,7 @@ function Home() {
          </div>
          
 {/* 2 */}
-          <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+          <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
             <img className='h-10' src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85NTM4NTEyZC1mZGUxLTRmNzMtYmQ1MS05Y2VmZjRlMjU0ZjEucG5n" alt="" />
              <div className='w-1/2'>
                 <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-line"></i>1</span></h4>
@@ -123,7 +124,7 @@ function Home() {
          </div>
 
 {/* 3 */}
-          <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+          <div className='flex border-2 active:border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
             <img className='h-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ46fNM1MPsh-kepvw7nyXhFLC7t93CemDKIA&s" alt="" />
              <div className='w-1/2'>
                 <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-line"></i>3</span></h4>
