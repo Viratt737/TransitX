a664afe5035afb1e8c1f3eb78584c0720f83d9da
virat <viratt737@gmail.com>
2026-04-04 03:15:48 +0530

    create dummy ui for user vehicle option where user choose vehicle for riding

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -40,7 +40,7 @@ function Home() {
 
 
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img
         className='w-16 absolute left-5 top-5'
         src="https://freelogopng.com/images/all_img/1659761100uber-logo-png.png"
@@ -99,7 +99,40 @@ function Home() {
         </div>
       </div>
       
-      
+      <div className='fixed z-10 bottom-0 w-full bg-white px-3 py-6'>
+         <h3 className='text-2xl font-semibold mb-5'>Choose a Vehicle</h3>
+         <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxQLjsY-4pvrxwCn0QWDCGljnRsqqc1CpF-Q&s" alt="" />
+             <div className='w-1/2'>
+                <h4 className='font-medium text-base'>UberGo <span><i className="ri-user-3-line"></i>4</span></h4>
+                <h5 className='font-medium text-base'>2 min away</h5>
+                <p className='font-medium text-xs text-gray-600'>Affordable, compact rides</p>
+             </div>
+             <h2 className='text-lg font-semibold'>Rs.193.12</h2>
+         </div>
+         
+{/* 2 */}
+          <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-10' src="https://cn-geo1.uber.com/image-proc/crop/resizecrop/udam/format=auto/width=552/height=552/srcb64=aHR0cHM6Ly90Yi1zdGF0aWMudWJlci5jb20vcHJvZC91ZGFtLWFzc2V0cy85NTM4NTEyZC1mZGUxLTRmNzMtYmQ1MS05Y2VmZjRlMjU0ZjEucG5n" alt="" />
+             <div className='w-1/2'>
+                <h4 className='font-medium text-base'>Moto <span><i className="ri-user-3-line"></i>1</span></h4>
+                <h5 className='font-medium text-base'>3 min away</h5>
+                <p className='font-medium text-xs text-gray-600'>Affordable, compact rides</p>
+             </div>
+             <h2 className='text-lg font-semibold'>Rs.69.12</h2>
+         </div>
+
+{/* 3 */}
+          <div className='flex border-2 border-black mb-2 rounded-xl w-full p-3 items-center justify-between'>
+            <img className='h-10' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ46fNM1MPsh-kepvw7nyXhFLC7t93CemDKIA&s" alt="" />
+             <div className='w-1/2'>
+                <h4 className='font-medium text-base'>UberAuto <span><i className="ri-user-3-line"></i>3</span></h4>
+                <h5 className='font-medium text-base'>4 min away</h5>
+                <p className='font-medium text-xs text-gray-600'>Affordable, compact rides</p>
+             </div>
+             <h2 className='text-lg font-semibold'>Rs.101.12</h2>
+         </div>
+      </div>
     </div>
   )
 }
