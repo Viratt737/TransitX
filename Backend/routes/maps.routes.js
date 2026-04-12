ac1bfb3889261e1948fbcee61e0ca732ad8daf68
virat <viratt737@gmail.com>
2026-04-12 10:13:48 +0530

    wriet code for calculate distance between two coordinate

--- Backend/routes/maps.routes.js
+++ Backend/routes/maps.routes.js
@@ -4,8 +4,15 @@ const authMiddleware = require('../middlewares/auth.middleware');
 const mapController = require('../controllers/maps.controllers');
 const {query} = require('express-validator')
 router.get('/get-coordinates',query('address').isString().isLength({min: 3}),
-    // authMiddleware.authUser, 
-    mapController.getCoordinates)
+    authMiddleware.authUser, 
+    mapController.getCoordinates);
+
+router.get('/get-distance-time',
+    query('origin').isString().isLength({min: 3}),
+    query('destination').isString().isLength({min: 3}),  // ← dot(.) se chain karo, comma nahi
+    authMiddleware.authUser,
+    mapController.getDistanceTime
+)
 
 
 module.exports = router;
